f16c0dbdd0ac1f114efceb399e4992952ed6097a
BarronWill <102401621+BarronWill@users.noreply.github.com>
2022-11-18 17:06:36 +0700

    Update O1.2.submit.cpp

--- O1.2.submit.cpp
+++ O1.2.submit.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<windows.h>
+#include<stdio.h>
 #include<iomanip>
 
 using namespace std;
@@ -72,17 +72,17 @@ void Phieu::get(){
     }
 }
 void Phieu::show(){
-    double Tong_Diem=0, Tong_so_trinh=0;
-    cout<<setw(50)<<"PHIEU KIEM TAI SAN\n\n";
+    double Tong_Strinh_Diem=0, Tong_so_trinh=0;
+    cout<<setw(50)<<"PHIEU DIEM\n\n";
     y.show();
     cout<<setw(15)<<"Ten Mon"<<setw(15)<<"So trinh"<<setw(15)<<"Diem"<<endl;
     for(int i=0;i<n;i++){
         Tong_so_trinh+=x[i].Sotrinh;
-        Tong_Diem+=x[i].Diem;
+        Tong_Strinh_Diem+=x[i].Diem*x[i].Sotrinh;
         x[i].show();
     }
     cout<<setw(44)<<"TB "<<fixed<<setprecision(2)
-    <<(float)Tong_Diem/Tong_so_trinh;
+    <<(float)Tong_Strinh_Diem/Tong_so_trinh;
 }
 
 void mor_3_ST(Phieu a){
@@ -97,7 +97,7 @@ void chen_them(Phieu& a){
     do{
     cout<<"Nhap vi tri muon chen: ";  cin>>k;
     k--;
-    }while(k<=0||k>=a.n);
+    }while(k<0||k>=a.n);
     a.x=(MonHoc*)realloc(a.x,(a.n+1)*sizeof(MonHoc));
     for(int i=a.n;i>k;i--){
         a.x[i] = a.x[i-1];
